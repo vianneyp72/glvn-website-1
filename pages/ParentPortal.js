a45548d8b88b1f49c54545d9b6bd6ef051e6f7ec
JonathanDinh <endrpanda@gmail.com>
2023-02-10 16:15:04 -0600

    Fixed hydration error with some edits to portals

--- pages/ParentPortal.js
+++ pages/ParentPortal.js
@@ -2,10 +2,10 @@ import Link from 'next/link'
 export default function ParentPortal () {
     return (
         <>
-            <h1>This is the teacher portal! (WIP)</h1>
-            <h2>
-                <Link href="/">Go back</Link>
-            </h2>
+            <h1>This is the parent portal! (WIP)</h1>
+            <div className="mt-8">
+                <Link href="/" className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Go back</Link>
+            </div>
         </>
     )
 }
--- pages/TeacherPortal.js
+++ pages/TeacherPortal.js
@@ -3,9 +3,9 @@ export default function TeacherPortal () {
     return (
         <>
             <h1>This is the teacher portal! (WIP)</h1>
-            <h2>
-                <Link href="/">Go back</Link>
-            </h2>
+            <div className="mt-8">
+                <Link href="/" className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Go back</Link>
+            </div>
         </>
     )
 }
