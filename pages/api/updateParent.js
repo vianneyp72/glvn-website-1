6307c720a24f2178d50da46baf0b8be61787e82b
Einwog <hung.pham@sasjhglvn.com>
2023-02-09 20:28:21 -0600

    fixed

--- pages/api/updateParent.js
+++ pages/api/updateParent.js
@@ -5,7 +5,6 @@ import { parentTable, getMinifiedRecord } from './utils/airtable';
 export default async (req, res) => {
     // const { user } = await auth0.getSession(req);
     const { id, fields } = req.body;
-    const {Father_Name, Mother_Name, Phone_1, Phone_2, Email_1, Email_2, Street_Address, City_State_Zip, Students_Link} = req.body;
     try {
         const updatedRecord = await parentTable.update([{id, fields}]);
         res.statusCode = 200;
