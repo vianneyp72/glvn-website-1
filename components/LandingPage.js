173b732ce04a792b54eddff9970f9205dcc00e29
JonathanDinh <endrpanda@gmail.com>
2023-02-10 14:21:16 -0600

    Added button functionality
    still broken with hydration failure

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,42 +1,45 @@
 import Image from 'next/image'
+import React from "react";
+import Link from 'next/link'
 
 function LandingPage() {
     return (
         <body>
-            <div
-                style={{display: "flex", justifyContent: "center",}}
-                className="flex">
-
-                <Image src="/GLVN_Minimal.png" alt="GLVN Icon" width={350} height={200}></Image>
+            <div className="flex justify-center">
+                <Image src="/GLVN_Minimal.png" alt="GLVN Icon" width={600} height={500}></Image>
             </div><br></br>
             <div className="flex justify-evenly">
-                {/*<button className="container bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 content-start">*/}
-                {/*    I am a button*/}
-                {/*</button>*/}
-                <div class="teacher-portal">
-                    <h1 className="text-3xl">Teacher Portal</h1>
-                    <div className="text-1xl text-center">
+                <div className="teacher-portal">
+                    <h1 className="text-4xl">Teacher Portal</h1>
+                    <div className="mt-4 text-2xl text-center">
                         <p>Class Lists</p>
                         <p>Attendance</p>
                         <p>Directory</p>
-                        <button className="container bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 content-start">
-                            Visit!</button>
+                        <div className="mt-8">
+                            <Link href="/TeacherPortal" className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Visit!</Link>
+                        </div>
                     </div>
                 </div>
-                <div class="parent-portal">
-                    <h1 className="text-3xl">Parent Portal</h1>
-                    <div className="text-1xl text-center">
+                <div className="parent-portal">
+                    <h1 className="text-4xl">Parent Portal</h1>
+                    <div className="mt-4 text-2xl text-center">
                         <p>Registration</p>
                         <p>Absent Student</p>
                         <p>Teacher Contact</p>
                         <p>Schedule/Calendar</p>
-                        <button className="container bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 content-start">
-                            Visit!</button>
+                        {/*<button className="container mt-4 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 content-start">*/}
+                        {/*    Visit!</button>*/}
+
+                        {/*<a href="/components/ParentPortal" className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Visit!</a>*/}
+
+                        <div className="mt-8">
+                            <Link href="/ParentPortal" className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Visit!</Link>
+                        </div>
                     </div>
                 </div>
             </div>
         </body>
-    )
+    );
 }
 
 export default LandingPage;
---
+++ pages/ParentPortal.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+export default function ParentPortal () {
+    return (
+        <>
+            <h1>This is the teacher portal! (WIP)</h1>
+            <h2>
+                <Link href="/">Go back</Link>
+            </h2>
+        </>
+    )
+}
