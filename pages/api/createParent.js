b3b41fc7da89e1d74ffdbb9da7c9b59f4c2c3466
Einwog <hung.pham@sasjhglvn.com>
2023-02-09 18:46:06 -0600

    Students_Link

--- pages/api/createParent.js
+++ pages/api/createParent.js
@@ -3,9 +3,9 @@ import { parentTable, getMinifiedRecord, minifyRecords } from './utils/airtable'
 
 export default async (req, res) => {
     // const { user } = await auth0.getSession(req);
-    const {Father_Name, Mother_Name, Phone_1, Phone_2, Email_1, Email_2, Street_Address, City_State_Zip} = req.body;
+    const {Father_Name, Mother_Name, Phone_1, Phone_2, Email_1, Email_2, Street_Address, City_State_Zip, Students_Link} = req.body;
     try {
-        const createdRecords = await parentTable.create([{fields: {Father_Name, Mother_Name, Phone_1, Phone_2, Email_1, Email_2, Street_Address, City_State_Zip}}]);
+        const createdRecords = await parentTable.create([{fields: {Father_Name, Mother_Name, Phone_1, Phone_2, Email_1, Email_2, Street_Address, City_State_Zip, Students_Link}}]);
         const createdRecord = {
             id: createdRecords[0].id,
             fields: createdRecords[0].fields
