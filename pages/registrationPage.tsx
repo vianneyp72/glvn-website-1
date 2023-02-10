4600070f6d1b39fac4f7e61e3970485acf964be7
vianneyp72 <vianneyp72@gmail.com>
2023-02-10 17:49:51 -0600

    migrated registration to its own page

--- pages/registrationPage.tsx
+++ pages/registrationPage.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <NavBar />
-      <div className='text-center font-bold my-10 text-3xl'>LANDING PAGE</div>
+      <RegForm />
     </main>
   )
 }
