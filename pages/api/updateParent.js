5eafc15a940d7099139eeaefe69ed3c752887d56
Einwog <hung.pham@sasjhglvn.com>
2023-02-09 20:47:18 -0600

    finally fixed updateParent working

--- pages/api/updateParent.js
+++ pages/api/updateParent.js
@@ -1,12 +1,11 @@
 import { parentTable, getMinifiedRecord } from './utils/airtable';
 // import auth0 from './utils/auth0';
-//wheeze number 2
 
 export default async (req, res) => {
     // const { user } = await auth0.getSession(req);
-    const { Family_ID, fields } = req.body;
+    const { id, fields } = req.body;
     try {
-        const updatedRecords = await parentTable.update([{Family_ID, fields}]);
+        const updatedRecords = await parentTable.update([{id, fields}]);
         res.statusCode = 200;
         res.json(getMinifiedRecord(updatedRecords[0]));
     } catch (error) {
