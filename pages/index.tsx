4a1224907ebe1ccbd0448863d7442a1c5c2ee2d3
vianneyp72 <vianneyp72@gmail.com>
2023-02-09 22:04:39 -0600

    update regform

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import NavBar from '../components/NavBar'
-import TestRegForm from '../components/RegForm'
+import RegForm from '../components/RegForm'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main>
       <NavBar />
-      <TestRegForm />
+      <RegForm />
     </main>
   )
 }
