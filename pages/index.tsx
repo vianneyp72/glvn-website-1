ac7d22d57c3a999587e25c80502768240c786cc8
JonathanDinh <endrpanda@gmail.com>
2023-02-10 18:01:56 -0600

    another index update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import NavBar from '../components/NavBar'
-import RegForm from '../components/RegForm'
 import LandingPage from '../components/LandingPage'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
