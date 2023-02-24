fa3272337b0a3a23999f0320f5921ad503cbd0f3
JonathanDinh <endrpanda@gmail.com>
2023-02-23 19:55:44 -0600

    Added bg-cover to all teacher portal subpages

--- pages/ClassListPage.js
+++ pages/ClassListPage.js
@@ -1,6 +1,6 @@
 export default function ClassListPage() {
     return (
-        <main className="bg-[url('/ClassListBackground.png')] h-screen w-screen">
+        <main className="bg-[url('/ClassListBackground.png')] bg-cover h-screen w-screen">
             <div>
                 <h1 className="flex py-8 px-8 flex-col items-center justify-between text-8xl font-bold">Class List</h1><br/>
             </div>
--- pages/DirectoryPage.js
+++ pages/DirectoryPage.js
@@ -1,6 +1,6 @@
 export default function DirectoryPage() {
     return (
-        <main className="bg-[url('/DirectoryBackground.png')] h-screen w-screen">
+        <main className="bg-[url('/DirectoryBackground.png')] bg-cover h-screen w-screen">
             <div>
                 <h1 className="flex py-8 px-8 flex-col items-center justify-between text-8xl font-bold">Directory</h1><br/>
             </div>
