a6f96da674a2e0163a4072aa46c3ce0b75935c38
vianneyp72 <vianneyp72@gmail.com>
2023-02-27 01:32:29 -0600

    multi student

--- components/RegFormTest.js
+++ components/RegFormTest.js
@@ -366,30 +366,202 @@ const Form = () => {
 
             {fields.map((field, index) => {
               return (
-                <section key={field.id}>
-                  <label>
-                    <span>Student's Saint Name</span>
-                    <input {...register(`students_cart.${index}.name`)} />
-                  </label>
-                  <label>
-                    <span>Student's First Name</span>
-                    <input {...register(`students_cart.${index}.amount`)} />
-                  </label>
-                  <label>
-                    <span>Student's Last Name</span>
-                    <input {...register(`students_cart.${index}.amount`)} />
-                  </label>
-                </section>
+                <>
+                  <section
+                    className="w-full border border-gray-200 p-2 rounded mb-3 bg-orange-200 shadow-md"
+                    key={field.id}
+                  >
+                    <div className="my-4">
+                      <label
+                        className="flex text-gray-700 font-medium mb-2"
+                        htmlFor="studentSaintName"
+                      >
+                        Student's Saint Name: <p className="text-red-500">*</p>
+                      </label>
+
+                      <input
+                        placeholder="ex. St.Thomas Aquinas"
+                        className="w-full border border-gray-400 p-2 rounded"
+                        type="text"
+                        name="studentSaintName"
+                        id="studentSaintName"
+                        {...register("studentSaintName", {
+                          required: true,
+                          pattern: {
+                            value: /^[A-Za-z.]+$/i,
+                            message: "Invalid Saint name",
+                          },
+                        })}
+                      />
+                      {errors.zipcode && (
+                        <span className="text-red-500">
+                          This field is required
+                        </span>
+                      )}
+                    </div>
+                    <label
+                      className="flex text-gray-700 font-medium mb-2"
+                      htmlFor="studentFirstName"
+                    >
+                      Student's Name:<p className="text-red-500">*</p>
+                    </label>
+                    <div className="flex space-x-3">
+                      <input
+                        placeholder="First Name"
+                        className="w-full border border-gray-400 p-2 rounded"
+                        type="text"
+                        name="studentFirstName"
+                        id="studentFirstName"
+                        {...register("studentFirstName", {
+                          required: true,
+                          pattern: {
+                            value: /^[A-Za-z.]+$/i,
+                            message: "Invalid Name",
+                          },
+                        })}
+                      />
+                      {errors.studentFirstName && (
+                        <span className="text-red-500">
+                          This field is required
+                        </span>
+                      )}
+                      <label
+                        className="block text-gray-700 font-medium mb-2"
+                        htmlFor="studentLastName"
+                      ></label>
+                      <input
+                        placeholder="Last Name"
+                        className="w-full border border-gray-400 p-2 rounded"
+                        type="text"
+                        name="studentLastName"
+                        id="studentLastName"
+                        {...register("studentLastName", {
+                          required: true,
+                          pattern: {
+                            value: /^[A-Za-z.]+$/i,
+                            message: "Invalid last name",
+                          },
+                        })}
+                      />
+                      {errors.studentLastName && (
+                        <span className="text-red-500">
+                          This field is required
+                        </span>
+                      )}
+                    </div>
+                    <div className="my-4">
+                      <label
+                        className="flex text-gray-700 font-medium mb-2"
+                        htmlFor="studentDOB"
+                      >
+                        Student's Date of Birth<p className="text-red-500">*</p>
+                      </label>
+                      <input
+                        className="w-full border border-gray-400 p-2 rounded"
+                        type="date"
+                        name="studentDOB"
+                        id="studentDOB"
+                        {...register("studentDOB", {
+                          required: true,
+                          pattern: {
+                            message: "Invalid Date",
+                          },
+                        })}
+                      />
+                      {errors.studentDOB && (
+                        <span className="text-red-500">
+                          This field is required
+                        </span>
+                      )}
+                    </div>
+                    <div className="my-4">
+                      <label
+                        className="flex text-gray-700 font-medium mb-2"
+                        htmlFor="studentBaptismDate"
+                      >
+                        Student's Baptism Date<p className="text-red-500">*</p>
+                      </label>
+                      <input
+                        className="w-full border border-gray-400 p-2 rounded"
+                        type="date"
+                        name="studentBaptismDate"
+                        id="studentBaptismDate"
+                        {...register("studentBaptismDate", {
+                          required: true,
+                          pattern: {
+                            message: "Invalid Date",
+                          },
+                        })}
+                      />
+                      {errors.studentBaptismDate && (
+                        <span className="text-red-500">
+                          This field is required
+                        </span>
+                      )}
+                    </div>
+                    <div className="my-4">
+                      <label
+                        className="block text-gray-700 font-medium mb-2"
+                        htmlFor="studentFirstCommunionDate"
+                      >
+                        Student's First Communion Date
+                      </label>
+                      <input
+                        className="w-full border border-gray-400 p-2 rounded"
+                        type="date"
+                        name="studentFirstCommunionDate"
+                        id="studentFirstCommunionDate"
+                        {...register("studentFirstCommunionDate", {
+                          required: false,
+                          pattern: {
+                            message: "Invalid Date",
+                          },
+                        })}
+                      />
+                      {errors.studentFirstCommunionDate && (
+                        <span className="text-red-500">
+                          This field is required
+                        </span>
+                      )}
+                    </div>
+                    <div className="flex flex-col items-end">
+                      <button
+                        className="bg-red-500 text-white px-2 rounded-lg hover:bg-red-600 font-bold"
+                        type="button"
+                        onClick={() => remove(index)}
+                      >
+                        <div className="flex">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                            />
+                          </svg>
+                          Remove Student
+                        </div>
+                      </button>
+                    </div>
+                  </section>
+                </>
               );
             })}
-
-            <button
-              className="bg-blue-500 text-white px-2 rounded-lg hover:bg-blue-600"
-              type="button"
-              onClick={() => append({})}
-            >
-              Add Student
-            </button>
+            <section className="pt-3">
+              <button
+                className="bg-blue-500 text-white px-2 rounded-lg hover:bg-blue-600 font-bold"
+                type="button"
+                onClick={() => append({})}
+              >
+                + Add Student
+              </button>
+            </section>
 
             {/* <div className="my-4">
               <label
