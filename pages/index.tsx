7a2a66af9919397c8e48e668d12dab6d424acd83
JonathanDinh <endrpanda@gmail.com>
2023-02-28 15:52:44 -0600

    Registration defaulted

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import LandingPage from '../components/LandingPage'
+import RegForm from "../components/RegForm";
+import NavBar from "../components/NavBarParents";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <main>
-        <LandingPage />
+        <NavBar/>
+        <RegForm/>
+        <Footer/>
     </main>
   )
 }
