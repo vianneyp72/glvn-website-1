371a600cb99ff8343cd5add7d5146fa9a5daa285
vianneyp72 <vianneyp72@gmail.com>
2023-02-28 15:26:50 -0600

    sign in

--- components/NavBarParents.js
+++ components/NavBarParents.js
@@ -107,6 +107,13 @@ export default function NavBar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <a
+                  href="/api/auth/login"
+                  className="text-white mr-3 hover:bg-onhover p-2 rounded-md"
+                >
+                  Sign In
+                </a>
+
                 <button
                   type="button"
                   className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
