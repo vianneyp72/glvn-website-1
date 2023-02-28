03fc16d331e27588a53ecd75a8e17d63dfd7313b
vianneyp72 <vianneyp72@gmail.com>
2023-02-28 15:34:32 -0600

    comment

--- components/NavBarParents.js
+++ components/NavBarParents.js
@@ -1,41 +1,41 @@
-// import React from "react";
-// import { useUser } from "@auth0/nextjs-auth0/client";
+import React from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
 import Image from "next/image";
-// import Link from "next/link";
+import Link from "next/link";
 
-function Navbar() {
-  const { user } = useUser();
-  return (
-    <nav className="flex justify-between items-center px-5 mx-10 py-2">
-      <div>
-        <Image
-          src="/GLVN_Minimal_small.png"
-          alt="GLVN Icon"
-          width={150}
-          height={150}
-        ></Image>
-      </div>
-      <div className="flex">
-        {user && (
-          <a
-            href="/api/auth/logout"
-            className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
-          >
-            Logout
-          </a>
-        )}
-        {!user && (
-          <a
-            href="/api/auth/login"
-            className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
-          >
-            Login
-          </a>
-        )}
-      </div>
-    </nav>
-  );
-}
+// function Navbar() {
+//   const { user } = useUser();
+//   return (
+//     <nav className="flex justify-between items-center px-5 mx-10 py-2">
+//       <div>
+//         <Image
+//           src="/GLVN_Minimal_small.png"
+//           alt="GLVN Icon"
+//           width={150}
+//           height={150}
+//         ></Image>
+//       </div>
+//       <div className="flex">
+//         {user && (
+//           <a
+//             href="/api/auth/logout"
+//             className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
+//           >
+//             Logout
+//           </a>
+//         )}
+//         {!user && (
+//           <a
+//             href="/api/auth/login"
+//             className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
+//           >
+//             Login
+//           </a>
+//         )}
+//       </div>
+//     </nav>
+//   );
+// }
 
 export default function NavBar() {
   return (
