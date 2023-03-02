68415c33aabdc7b66405f5785c138e0d78408e71
vianneyp72 <vianneyp72@gmail.com>
2023-03-02 14:54:43 -0600

    logos

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function NavBar() {
   return (
     <footer class="p-4 shadow md:flex md:items-center md:justify-between md:p-6 bg-primary border-t border-gray-700">
@@ -8,6 +10,23 @@ export default function NavBar() {
         </a>
         <a>. All Rights Reserved.</a>
       </span>
+      <ul className="flex flex-wrap items-center">
+        <li className="m-3">
+          <Image src="/Azure.png" width={100} height={100} />
+        </li>
+        <li className="m-3">
+          <Image src="/Airtable.png" width={100} height={100} />
+        </li>
+        <li className="m-3">
+          <Image src="/Discord.png" width={100} height={100} />
+        </li>
+        <li className="m-3">
+          <Image src="/Microsoft.png" width={100} height={100} />
+        </li>
+        <li className="m-3">
+          <Image src="/Zapier.png" width={100} height={100} />
+        </li>
+      </ul>
       <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
           <a href="#" class="mr-4 hover:underline md:mr-6 ">
