9e1756ae877a7b67808f60c51a8c560e9668777f
vianneyp72 <vianneyp72@gmail.com>
2023-03-03 16:09:17 -0600

    start

--- pages/family-profile-page.tsx
+++ pages/family-profile-page.tsx
@@ -1,8 +1,12 @@
 import NavBar from "../components/NavBarParents";
 import FamProfile from "../components/FamProfile";
 import Footer from "../components/Footer";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function RegForm() {
+  const {user, isLoading, error} = useUser();
+  console.log("USER INFO:",user)
+  console.log("MYID:",user?.sid)
   return (
     <main>
       <NavBar />
