ff20ae7229ecbe074a585b9d67dd62fbd656ee2d
vianneyp72 <vianneyp72@gmail.com>
2023-03-03 10:22:55 -0600

    updated welcome

--- components/RegWelcome.js
+++ components/RegWelcome.js
@@ -5,12 +5,12 @@ export default function RegWelcome() {
       <div className="flex justify-center">
         <div className="bg-secondary shadow-lg p-10 rounded-lg w-2/3 sm:w-1/2 lg:w-1/3 text-center">
           <h3 className="font-bold text-4xl sm:text-6xl pb-10">Welcome!</h3>
-          <p1 className="text-primarytext text-xs sm:text-lg">
+          <p1 className="text-primarytext text-xs sm:text-lg font-bold">
             Create an Account.
             <br />
             Setup your family profile.
             <br />
-            Register your kids for the following school year!
+            Register for the school year!
           </p1>
           <br />
           <div className="border border-gray-700 mt-10 mb-10"></div>
