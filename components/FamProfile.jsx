45ea96586b0ceb18296a720e12ebc67d20080174
vianneyp72 <vianneyp72@gmail.com>
2023-03-04 03:39:34 -0600

    state working

--- components/FamProfile.jsx
+++ components/FamProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { parentTable } from "../pages/api/utils/airtable";
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0/client";
@@ -246,7 +246,7 @@ const FamProfile = () => {
                 {...register("pg1_phone", {
                   required: true,
                   pattern: {
-                    value: /^[0-9]{14}$/i,
+                    value: /^[0-9() -]/i,
                     message: "Invalid phone number",
                   },
                 })}
@@ -454,20 +454,65 @@ const FamProfile = () => {
                     >
                       State:<p className="text-red-500">*</p>
                     </label>
-                    <input
-                      placeholder="MN"
+
+                    <select
                       className="w-full border bg-tertiary border-fourth p-2 rounded"
-                      type="text"
-                      name="state"
                       id="state"
-                      {...register("state", {
-                        required: true,
-                        pattern: {
-                          value: /^[A-Za-z]+$/i,
-                          message: "invalid State name",
-                        },
-                      })}
-                    />
+                      {...register("state", { required: true })}
+                      required
+                    >
+                      <option value="">Select a state</option>
+                      <option value="AL">Alabama</option>
+                      <option value="AK">Alaska</option>
+                      <option value="AZ">Arizona</option>
+                      <option value="AR">Arkansas</option>
+                      <option value="CA">California</option>
+                      <option value="CO">Colorado</option>
+                      <option value="CT">Connecticut</option>
+                      <option value="DE">Delaware</option>
+                      <option value="FL">Florida</option>
+                      <option value="GA">Georgia</option>
+                      <option value="HI">Hawaii</option>
+                      <option value="ID">Idaho</option>
+                      <option value="IL">Illinois</option>
+                      <option value="IN">Indiana</option>
+                      <option value="IA">Iowa</option>
+                      <option value="KS">Kansas</option>
+                      <option value="KY">Kentucky</option>
+                      <option value="LA">Louisiana</option>
+                      <option value="ME">Maine</option>
+                      <option value="MD">Maryland</option>
+                      <option value="MA">Massachusetts</option>
+                      <option value="MI">Michigan</option>
+                      <option value="MN">Minnesota</option>
+                      <option value="MS">Mississippi</option>
+                      <option value="MO">Missouri</option>
+                      <option value="MT">Montana</option>
+                      <option value="NE">Nebraska</option>
+                      <option value="NV">Nevada</option>
+                      <option value="NH">New Hampshire</option>
+                      <option value="NJ">New Jersey</option>
+                      <option value="NM">New Mexico</option>
+                      <option value="NY">New York</option>
+                      <option value="NC">North Carolina</option>
+                      <option value="ND">North Dakota</option>
+                      <option value="OH">Ohio</option>
+                      <option value="OK">Oklahoma</option>
+                      <option value="OR">Oregon</option>
+                      <option value="PA">Pennsylvania</option>
+                      <option value="RI">Rhode Island</option>
+                      <option value="SC">South Carolina</option>
+                      <option value="SD">South Dakota</option>
+                      <option value="TN">Tennessee</option>
+                      <option value="TX">Texas</option>
+                      <option value="UT">Utah</option>
+                      <option value="VT">Vermont</option>
+                      <option value="VA">Virginia</option>
+                      <option value="WA">Washington</option>
+                      <option value="WV">West Virginia</option>
+                      <option value="WI">Wisconsin</option>
+                      <option value="WY">Wyoming</option>
+                    </select>
                     {errors.state && (
                       <span className="text-red-500">
                         This field is required
@@ -489,10 +534,10 @@ const FamProfile = () => {
                       name="zipcode"
                       id="zipcode"
                       {...register("zipcode", {
-                        required: true,
                         pattern: {
-                          value: /^[0-9]{5}$/i,
+                          value: /^[\s*\d{5}(-\d{4})?\s*$]/i,
                           message: "invalid zipcode",
+                          required: true,
                         },
                       })}
                     />
